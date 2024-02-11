a2b68b845aa169c6b66737af2c144bfbe37a7842
TanyaLark <tanyaenlang9898@gmail.com>
2024-02-11 21:07:35 +0200

    Added start logic

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
