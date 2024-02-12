7ddd20e9b136fab45b76bfb4189526c329e46f34
TanyaLark <tanyaenlang9898@gmail.com>
2024-02-12 11:08:36 +0200

    Add GET endpoint for retrieving a specific tour by ID

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/api/v1/tours', (req, res) => {
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
-  
+
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -42,6 +42,25 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour,
+    },
+  });
 });
 
 const port = 3000;
