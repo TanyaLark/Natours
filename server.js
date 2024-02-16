682810c1a9b902ac496da4fce9c00a1863c6191a
TanyaLark <tanyaenlang9898@gmail.com>
2024-02-16 12:14:35 +0200

    Added local database connection opportunity

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+// const DB_LOCAL = process.env.DATABASE_LOCAL;
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-    // useUnifiedTopology: true
   })
   .then(() => {
     console.log('DB connection successful');
